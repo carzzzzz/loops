17cf5bbc8cee2729febf2d61b092c28d8747198b
carzzzzz <113055449+carzzzzz@users.noreply.github.com>
2022-09-21 14:30:33 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index <= 9; index++) {
+        basic.showString("Hi")
+        basic.clearScreen()
+        basic.pause(100)
+    }
+})
+basic.forever(function () {
+	
+})
